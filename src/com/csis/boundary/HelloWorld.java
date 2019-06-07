fa02500edef9548892e1d2c41f1a32961e763cdc
Domingues Rocha de Oliveira <dominguesrochadasilveiras@student.douglascollege.ca>
2019-06-07 11:18:06 -0700

    Converted HelloWorld

--- src/com/csis/boundary/HelloWorld.java
+++ src/com/csis/boundary/HelloWorld.java
@@ -1,9 +1,44 @@
 package com.csis.boundary;
 
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class HelloWorld {
 
-	//Samuel
-	//Victor
-	//Chhavi
-	
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HelloWorld window = new HelloWorld();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HelloWorld() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 }
